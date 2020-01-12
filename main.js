7c9ae64372f0441ca3f906c26e7b04256b02db37
Nick Woods <nick.woods.wi@gmail.com>
2020-01-11 18:17:59 -0600

    updated main.js

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-//STYLES SITE NAVIGATION FOR MOBILE DEVICES
 function menuDrop() {
   $('.menu-button-container').on('click', function() {
     $('.navwrapper').toggleClass('mobileNav');
   });
 }
 
-//LOADS INITIAL SEARCH RESULTS TO FRONT PAGES
+function neighborhoodsDrop() {
+  $('#neighborhoods').on('click', function() {
+   let text = document.getElementById('neighborhoods');
+    if (text.innerHTML === '<a href="#" onclick="return false;">+ HOMES FOR SALE</a>') {
+      text.innerHTML = '<a href="#" onclick="return false;">- HOMES FOR SALE</a>'
+    } else {text.innerHTML = '<a href="#" onclick="return false;">+ HOMES FOR SALE</a>'};
+    $('.neighborhoodsHidden').toggleClass('neighborhoodsOpen');
+  })
+};
+
 let searchUrl =
   'https://www.firstweber.com/homes-for-sale/Active%2CActiveWO%2CDelayed_list-status/68783_listing-sale-agents/pa_68783/nts_12/sd_S2/sc_lsearch_amt_search_price%2BDESC/';
 
-//SETS MARKETS FOR SEARCH FROM BROKER WEBSITE
 const neighborhoods = {
   '#mequon':
     'https://www.firstweber.com/homes-for-sale/Mequon_combo/P_lp-cd-presentation/sc_lsearch_amt_search_price+DESC/sd_S2/page_1//nts_100/',
@@ -29,13 +36,12 @@ const neighborhoods = {
     'https://www.firstweber.com/homes-for-sale/bayside_combo/P_lp-cd-presentation/sc_lsearch_amt_search_price+DESC/sd_S2/page_1//nts_100/',
 };
 
-//LOADS AND FORMATS FRONT-END CODE FROM BROKER SEARCH RESULTS
 function loadPropertyDetails(url) {
   fetch(url)
-    .then((response) => {
-      return response.text();
-    })
-    .then ((pageSource) => {
+  .then((response) => {
+    return response.text();
+  })
+  .then ((pageSource) => {
     const properties = [];
     $.each($(pageSource), function(_, element) {
       if (element.id === 'pagewrap') {
@@ -87,8 +93,6 @@ function loadPropertyDetails(url) {
     buildPreviews(properties);
   });
 }
-
-//BUILDS DISPLAYS IN THE DOM FROM FRONT END SEARCH CODE
 function buildPreviews(properties) {
   const payload = properties.map(property => {
     const { city, price, street, link, photo, streetSubj } = property;
@@ -115,8 +119,6 @@ function buildPreviews(properties) {
   });
   $('.listings').html(payload);
 }
-
-//ENABLES THE USER TO LOAD A NEW SEARCH
 function chooseNeighborhood() {
   Object.keys(neighborhoods).forEach(id => {
     $(id).on('click', function() {
